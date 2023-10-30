a9f8a15c99c48b8284c62927bfa5c357283294ab
cogicgoran <cogicgoran99@gmail.com>
2023-10-30 10:21:11 +0100

    Add pagination for businesses table

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -24,18 +24,53 @@ export class DashboardComponent implements OnInit, OnDestroy {
     numberOfEmployees: number;
   }>
   subscription!: Subscription;
+  currentPage = 1;
+  articlesPerPage = 3;
+  totalPages = 1;
+  businessesCount = 0;
 
   constructor(private businessService: BusinessService, private router: Router) { }
 
   ngOnInit(): void {
     this.subscription = this.businessService.myBusinessObservable.subscribe((businesses) => {
-      this.businesses = businesses.map((business) => {
-        return {
-          id: business.id,
-          name: business.name,
-          numberOfEmployees: business.employees.length
-        }
-      });
+      this.businessesCount = businesses.length;
+      this.totalPages = Math.ceil(this.businessesCount / this.articlesPerPage) || 1;
+      if(this.currentPage > this.totalPages) this.currentPage = this.totalPages;
+      this.businesses = businesses
+        .slice((this.currentPage - 1) * this.articlesPerPage, this.currentPage * this.articlesPerPage)
+        .map((business) => {
+          return {
+            id: business.id,
+            name: business.name,
+            numberOfEmployees: business.employees.length
+          }
+        });
+    });
+  }
+
+  nextPage() {
+    if (this.currentPage === this.totalPages) return;
+    this.currentPage = this.currentPage + 1;
+    this.businesses = this.businessService.myBusinesses.value.slice((this.currentPage - 1) * this.articlesPerPage, this.currentPage * this.articlesPerPage)
+    .map((business) => {
+      return {
+        id: business.id,
+        name: business.name,
+        numberOfEmployees: business.employees.length
+      }
+    });
+  }
+
+  prevPage() {
+    if (this.currentPage === 1) return;
+    this.currentPage = this.currentPage - 1;
+    this.businesses = this.businessService.myBusinesses.value.slice((this.currentPage - 1) * this.articlesPerPage, this.currentPage * this.articlesPerPage)
+    .map((business) => {
+      return {
+        id: business.id,
+        name: business.name,
+        numberOfEmployees: business.employees.length
+      }
     });
   }
 
